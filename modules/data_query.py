ef2c6855b321a574de9f141695358531196010be
Suhas Sunder <suhas@live.ca>
2025-03-30 18:49:35 -0400

    fix: meals logged
    -Fix issue with meals logged method throwing an error due to data mis-match between food and exercise.

--- modules/data_query.py
+++ modules/data_query.py
@@ -25,25 +25,31 @@ class QueryData:
   def display_list_of_meals(self, list_of_foods):
     # Display the list of foods queried
     for index, food in enumerate(list_of_foods):
-      print(f"{index + 1}. {color_text(food["name"], "BRIGHT_ORANGE", True)} | Serving size: {color_text(food['serving_size'], 'BRIGHT_PURPLE', True)} | " + color_text("Calories", "BRIGHT_PINK", True) + f" per serving: {color_text(food['calories'], 'BRIGHT_PINK', True)} | " + color_text("Grams", "BRIGHT_CYAN", True) + f" per serving: {color_text(food['grams'], 'BRIGHT_CYAN', True)}", )
+      print(
+        f"{index + 1}. {color_text(food['name'], 'BRIGHT_ORANGE', True)} | "
+        f"Serving size: {color_text(food['serving_size'], 'BRIGHT_PURPLE', True)} | "
+        f"{color_text('Calories', 'BRIGHT_PINK', True)} per serving: {color_text(food['calories'], 'BRIGHT_PINK', True)} | "
+        f"{color_text('Grams', 'BRIGHT_CYAN', True)} per serving: {color_text(food['grams'], 'BRIGHT_CYAN', True)}"
+      )
+
     
     print("")       
     
   def display_list_of_exercises(self, list_of_exercises):
     # Display the list of exercises queried
     for index, exercise in enumerate(list_of_exercises):
-      print(f"{index + 1}. {color_text(exercise["name"], "BRIGHT_ORANGE", True)} | Minutes Spent: {color_text(exercise['minutes_exercised'], 'BRIGHT_PURPLE', True)} | " + color_text("Calories Burned", "BRIGHT_PINK", True) + f": {color_text(exercise['calories_burned'], 'BRIGHT_PINK', True)}", )
+      print(f"{index + 1}. {color_text(exercise["name"], "BRIGHT_ORANGE", True)} | Minutes Spent: {color_text(exercise['minutes_exercised'], 'BRIGHT_PURPLE', True)} | " + color_text("Calories Burned", "BRIGHT_PINK", True) + f": {color_text(exercise['calories_burned'], 'BRIGHT_PINK', True)}")
       
     print("")           
         
   def meals_by_date(self, target_year, target_month, target_day):   
     # Load ANSI color codes from a JSON file
-    with open('data/exercise_log.json') as file:
-        exercise_log = json.load(file)
+    with open('data/food_log.json') as file:
+        food_log = json.load(file)
         
     date = f"{target_year}-{target_month}-{target_day}"
     
-    meals_logged = self.extract_data_by_date(date, exercise_log)
+    meals_logged = self.extract_data_by_date(date, food_log)
     
     self.display_list_of_meals(meals_logged)   
       
